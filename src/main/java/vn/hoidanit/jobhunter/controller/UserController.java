3954535b159dcb33e272e7d6b8e307d664b1bad2
La Anh Quan <laanhquan1807@gmail.com>
2025-08-22 13:32:05 +0700

    Xu ly exception

--- src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -1,10 +1,21 @@
 package vn.hoidanit.jobhunter.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.jobhunter.domain.User;
 import vn.hoidanit.jobhunter.service.UserService;
+import vn.hoidanit.jobhunter.service.error.IdInvalidException;
 
 @RestController
 public class UserController {
@@ -15,16 +26,45 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/user/create")
-    public String createNewUser() {
-        User user = new User();
-        user.setEmail("laanhquan1807@gmail.com");
-        user.setName("laquan");
-        user.setPassword("123456");
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> fetchUserById() {
 
-        this.userService.handleCreateUser(user);
+        List<User> userNew = this.userService.handleFetchAllUser();
 
-        return "create_user";
+        return ResponseEntity.status(HttpStatus.OK).body(userNew);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> fetchUserById(@PathVariable("id") long id) {
+
+        User userNew = this.userService.handleFetchUserById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(userNew);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createNewUser(@RequestBody User user) {
+
+        User userNew = this.userService.handleCreateUser(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(userNew);
+    }
+
+    @PutMapping("/users")
+    public ResponseEntity<User> updateUser(@RequestBody User user) {
+
+        User userNew = this.userService.handleCreateUser(user);
+
+        return ResponseEntity.status(HttpStatus.OK).body(userNew);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id") long id) throws IdInvalidException {
+        if (id >= 1500) {
+            throw new IdInvalidException("Id khong lon hon 1500");
+        }
+        this.userService.handleDeleteUser(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Delete successfully");
     }
 
 }
--- src/main/java/vn/hoidanit/jobhunter/service/UserService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/UserService.java
@@ -1,5 +1,8 @@
 package vn.hoidanit.jobhunter.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.jobhunter.domain.User;
@@ -13,7 +16,33 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void handleCreateUser(User user) {
-        this.userRepository.save(user);
+    public User handleCreateUser(User user) {
+        return this.userRepository.save(user);
+    }
+
+    public void handleDeleteUser(long id) {
+
+        this.userRepository.deleteById(id);
+    }
+
+    public User handleFetchUserById(long id) {
+        Optional<User> userOptional = this.userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        }
+        return null;
+    }
+
+    public Optional<User> handleUpdateUser(User user) {
+        return this.userRepository.findById(user.getId()).map(user1 -> {
+            user1.setName(user.getName());
+            user1.setEmail(user.getEmail());
+            user1.setPassword(user.getPassword());
+            return userRepository.save(user1);
+        });
+    }
+
+    public List<User> handleFetchAllUser() {
+        return this.userRepository.findAll();
     }
 }
